2369b1ed0a842b765200418f1099674aee8030e7
Sapana Kale <44019343+sapanaKale@users.noreply.github.com>
2019-01-16 14:23:01 +0530

    ReadFileSync is replaced with readFile.
    server needs to handle the invalid requests.
    and needs to remove the duplication from app.js

--- src/app.js
+++ src/app.js
@@ -1,39 +1,49 @@
 const fs = require('fs');
 
 const app = (req, res) => {
-	const homePage = fs.readFileSync('./src/homePage.html').toString();
-	if(req.url == "/main.js"){
-		res.statusCode = 200;
-		res.write(fs.readFileSync('./src/main.js').toString());
-		res.end();
-		return;
+	if (req.url == "/main.js") {
+		fs.readFile('./src/main.js', "utf8", (err, content) => {
+			res.statusCode = 200;
+			res.write(content);
+			res.end();
+			return;
+		})
 	}
-	if(req.url == "/main.css"){
-		res.statusCode = 200;
-		res.write(fs.readFileSync('./src/main.css').toString());
-		res.end();
-		return;
+	if (req.url == "/main.css") {
+		fs.readFile('./src/main.css', 'utf8', (err, content) => {
+			res.statusCode = 200;
+			res.write(content);
+			res.end();
+			return;
+		})
 	}
-	if(req.url == "/fc-sapanaKale/src/images/freshorigins.jpg"){
-		res.statusCode = 200;
-		res.write(fs.readFileSync('./src/images/freshorigins.jpg'));
-		res.end();
-		return;
+	if (req.url == "/fc-sapanaKale/src/images/freshorigins.jpg") {
+		fs.readFile('./src/images/freshorigins.jpg', (err, content) => {
+			res.statusCode = 200;
+			res.write(content);
+			res.end();
+			return;
+		})
 	}
-	if(req.url == "/fc-sapanaKale/src/images/animated-flower-image-0021.gif"){
-		res.statusCode = 200;
-		res.write(fs.readFileSync('./src/images/animated-flower-image-0021.gif'));
-		res.end();
-		return;
+	if (req.url == "/fc-sapanaKale/src/images/animated-flower-image-0021.gif") {
+		fs.readFile('./src/images/animated-flower-image-0021.gif', (err, content) => {
+			res.statusCode = 200;
+			res.write(content);
+			res.end();
+			return;
+		})
 	}
-	if(req.url == "/"){
-		res.statusCode = 200;
-		res.write(homePage);
-		res.end();
-		return;
+	if (req.url == "/") {
+		fs.readFile('./src/homePage.html', "utf8", (err, content) => {
+			res.statusCode = 200;
+			res.write(content);
+			res.end();
+			return;
+		})
 	}
-  res.statusCode = 404;
-  res.end();
+	else {
+		res.statusCode = 404;
+	}
 };
 
 module.exports = app;
